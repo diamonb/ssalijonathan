a1194d39566f5d9a577ddd9cca188de761a2bd9e
Magnima Wilfried MAYABA <mmayaba@OTR.LAN>
2026-02-26 18:27:00 +0100

    26022026

--- backend/api.py
+++ backend/api.py
@@ -1,10 +1,11 @@
 from flask import Flask,request,jsonify
 from flask_restx import Api,Resource,fields
 from config import DevConfig
-from model import Recipe
+from model import Recipe,User
 from exts import db
 import json
 from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 
@@ -21,11 +22,51 @@ Recipe_model = api.model("Recipe",
                              "title": fields.String,
                              "description": fields.String
                          })
+
+Sign_up_model = api.model("Sign_up",
+                         {
+                             "username": fields.String,
+                             "email": fields.String,
+                             "password" :fields.String
+                         })
+
 @api.route('/accueil')
 class HelloRessource(Resource):
     def get(self):
         return "<p>Accueil</p>"
     
+@api.route('/signup')
+class SignupRessource(Resource):
+    @api.marshal_list_with(Sign_up_model)
+    def get(self):
+        users = User.query.all()
+        return users
+    @api.marshal_with(Sign_up_model)
+    @api.expect(Sign_up_model)
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        email = data.get('email')
+        password = data.get('password')
+        db_user = User.query.filter(User.username==username).first()
+        if db_user :
+            return jsonify({'message':f'Utilisateur {username} existe déjà'})
+        new_user = User(
+            username = username,
+            email = email,
+            password = generate_password_hash(password)
+        )
+        new_user.save()
+        return new_user
+    
+@api.route('/login')
+class LoginRessource(Resource):
+    pass
+    
+@api.route('/accueil')
+class HelloRessource(Resource):
+    def get(self):
+        return "<p>Accueil</p>"
 @api.route('/recipes')
 class RecipesRessource(Resource):
     @api.marshal_list_with(Recipe_model)
@@ -33,7 +74,8 @@ class RecipesRessource(Resource):
         recipes = Recipe.query.all()
         return recipes
     
-    @api.marshal_list_with(Recipe_model)
+    @api.marshal_with(Recipe_model)
+    @api.expect(Recipe_model)
     def post(self):
         """ Post recipe """
 
@@ -47,12 +89,12 @@ class RecipesRessource(Resource):
     
 @api.route('/recipe/<int:id>')
 class RecipeRessource(Resource):
-    @api.marshal_list_with(Recipe_model)
+    @api.marshal_with(Recipe_model)
     def get(self,id):
         """"Get a recipe"""
         recipe = Recipe.query.get_or_404(id)
         return recipe
-    @api.marshal_list_with(Recipe_model)    
+    @api.marshal_with(Recipe_model)    
     def put(self,id):
         """"Update a recipe"""        
         recipe_to_update = Recipe.query.get_or_404(id)
@@ -60,7 +102,7 @@ class RecipeRessource(Resource):
         recipe_to_update.update(data.get('title'),data.get('description'))
         return recipe_to_update
     
-    @api.marshal_list_with(Recipe_model)    
+    @api.marshal_with(Recipe_model)    
     def delete(self,id):
         """"Delete a recipe"""        
         recipe_to_delete = Recipe.query.get_or_404(id)
--- backend/model.py
+++ backend/model.py
@@ -40,8 +40,8 @@ Class User:
 """
 class User(db.Model):
     id = db.Column(db.Integer(), primary_key = True)
-    username = db.Column(db.String(25), nullable = False)
-    email =  db.Column(db.String(80), nullable = False)
+    username = db.Column(db.String(25), nullable = False, unique = True)
+    email =  db.Column(db.String(80), nullable = False, unique = True)
     password =  db.Column(db.String(), nullable = False)
     def __repr__(self):
         return f'User {self.title}'
